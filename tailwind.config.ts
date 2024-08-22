6ae6515d259ff2526a242d98bf94c7a7fef91510
Vannn03 <90308438+Vannn03@users.noreply.github.com>
2024-08-22 13:10:19 +0700

    fix: tailwind not rendering

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,12 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/contexts/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {},
+  },
   plugins: [
     require('daisyui'),
   ],
